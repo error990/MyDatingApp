0ea2132ec559d185bfb0410a9fa70cad09779893
Florian Wagner <fehlermeldung@hotmail.com>
2023-10-25 11:08:15 +0200

    End of Section 5

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+import { Observable, of } from 'rxjs';
+import { User } from '../_models/user';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+
+export class NavComponent implements OnInit {
+
+  model: any = { };
+
+  constructor(public accountService: AccountService) { }
+
+  ngOnInit(): void { }
+
+  login(){
+    this.accountService.login(this.model).subscribe({
+      next: response => {
+        console.log(response);
+      },
+      error: err => console.log(err),
+    }
+    );
+  }
+
+  logout(){
+    this.accountService.logout();
+  }
+}
